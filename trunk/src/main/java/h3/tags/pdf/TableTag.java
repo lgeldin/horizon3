1ffad66ecbd3b001fe9e59b470cb90b812ff4c90
lgeldin2 <lgeldin2@a7a09280-c2fc-11de-a8ef-132414f8baa6>
2009-11-22 19:52:27 +0000

    report filter

--- trunk/src/main/java/h3/tags/pdf/TableTag.java
+++ trunk/src/main/java/h3/tags/pdf/TableTag.java
@@ -1,6 +1,8 @@
 package h3.tags.pdf;
 
 import h3.services.DataStoreService;
+import h3.state.BaseState;
+import h3.state.DataStoreState;
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -29,13 +31,41 @@ public class TableTag extends SimplePDFTag {
 	public void doTag(XMLOutput arg0) throws MissingAttributeException,
 			JellyTagException {
 		super.doTag(arg0);
+		Document doc = (Document) getContext().getVariable("document");
 		DataStoreService storeService = (DataStoreService) getContext()
 				.getVariable("storeService");
+		
 		String storeId = (String) getAttrs().get("store");
 		String screenName = (String) getContext().getVariable("screenName");
 		List<Map<String, Object>> data = (List<Map<String, Object>>) storeService
 				.getData(screenName, storeId);
 		PdfPTable table = new PdfPTable(columns.size());
+		
+		BaseState screenState = (BaseState) getContext().getVariable("screenState");
+		DataStoreState storeState = (DataStoreState) screenState.getWidgetState(storeId);
+		Paragraph filterString = null;
+		
+		if (storeState.getFilterState()!=null) {
+			String result = "Filter : ";
+			boolean filtered = false;
+			for (Map headers : columns) {
+				String dataIndex = (String) headers.get("dataIndex");
+				String filterValue = storeState.getFilterState().getFieldState(dataIndex);
+				
+				if (filterValue!=null) {
+					if (filtered)
+						result += ",";
+					result += dataIndex + "=" + filterValue	 + "";
+					filtered = true;
+				}
+			}
+			if (filtered) {
+				filterString = new Paragraph(result);
+				filterString.setSpacingAfter(10);
+				filterString.setIndentationLeft(10);
+			}
+		}
+		
 		for (Map headers : columns) {
 			addCell(table, headers.get("header"), true);
 		}
@@ -46,22 +76,9 @@ public class TableTag extends SimplePDFTag {
 			}
 		}
 
-		/*
-		 * PdfPCell cell = new PdfPCell (new Paragraph ("Rose India"));
-		 * cell.setColspan (2); cell.setHorizontalAlignment
-		 * (Element.ALIGN_CENTER); cell.setBackgroundColor (new Color (128, 200,
-		 * 128)); cell.setPadding (10.0f); table.addCell (cell); cell = new
-		 * PdfPCell (new Paragraph ("Name")); cell.setHorizontalAlignment
-		 * (Element.ALIGN_CENTER); cell.setBackgroundColor (new Color (255, 200,
-		 * 0)); cell.setPadding (10.0f); table.addCell (cell); cell = new
-		 * PdfPCell (new Paragraph ("Place")); cell.setHorizontalAlignment
-		 * (Element.ALIGN_CENTER); cell.setBackgroundColor (new Color (255, 200,
-		 * 0)); cell.setPadding (10.0f); table.addCell (cell);
-		 * table.addCell("NewsTrack"); table.addCell("Delhi");
-		 * table.addCell("RoseIndia"); table.addCell("Delhi " + data.size());
-		 */
-		Document doc = (Document) getContext().getVariable("document");
 		try {
+			if (filterString !=null)
+				doc.add(filterString);
 			doc.add(table);
 		} catch (DocumentException e) {
 			throw new JellyTagException(e);
@@ -82,3 +99,17 @@ public class TableTag extends SimplePDFTag {
 
 	}
 }
+/*
+ * PdfPCell cell = new PdfPCell (new Paragraph ("Rose India"));
+ * cell.setColspan (2); cell.setHorizontalAlignment
+ * (Element.ALIGN_CENTER); cell.setBackgroundColor (new Color (128, 200,
+ * 128)); cell.setPadding (10.0f); table.addCell (cell); cell = new
+ * PdfPCell (new Paragraph ("Name")); cell.setHorizontalAlignment
+ * (Element.ALIGN_CENTER); cell.setBackgroundColor (new Color (255, 200,
+ * 0)); cell.setPadding (10.0f); table.addCell (cell); cell = new
+ * PdfPCell (new Paragraph ("Place")); cell.setHorizontalAlignment
+ * (Element.ALIGN_CENTER); cell.setBackgroundColor (new Color (255, 200,
+ * 0)); cell.setPadding (10.0f); table.addCell (cell);
+ * table.addCell("NewsTrack"); table.addCell("Delhi");
+ * table.addCell("RoseIndia"); table.addCell("Delhi " + data.size());
+ */
